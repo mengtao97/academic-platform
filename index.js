916d52681de3052516e6a7c2fbef28b7213b6ad3
chencongyong <ndsf@qq.com>
2019-11-30 16:33:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // @ts-check
 
-const { ApolloServer, PubSub } = require('apollo-server-express');
+const {ApolloServer, PubSub} = require('apollo-server-express');
 const mongoose = require('mongoose');
 const express = require('express');
 
@@ -13,35 +13,18 @@ const pubsub = new PubSub();
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req }) => ({ req, pubsub })
+    context: ({req}) => ({req, pubsub})
 });
 
 const app = express();
 
-server.applyMiddleware({ app, path: '/' });
+server.applyMiddleware({app, path: '/'});
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
-async function establishMongooseConnection() {
-    const connection = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-        console.log('MongoDB connected');
-        return app.listen({ port }, () => {
-            console.log(`Server running at http://localhost:${port}`);
-        });
-    })
-    return connection
-}
-
-establishMongooseConnection()
-// const userModel = require("./models/User");
-// const paperModel = require("./models/Paper");
-// const loadExampleData = require('./util/loadData');
-
-// async function _test() {
-//     await establishMongooseConnection();
-//     const [authors, papers] = loadExampleData();
-//     userModel.collection.insertMany(authors).then(() => console.log("Finished!"));
-//     paperModel.collection.insertMany(papers);
-// }
-
-// _test();
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log('MongoDB connected');
+    return app.listen({port}, () => {
+        console.log(`Server running at http://localhost:${port}`);
+    });
+});
