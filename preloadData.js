2f369bd87eb73cd12a6bfc2c2b36850ea25f243a
Vian Chen <imvianchen@outlook.com>
2019-12-11 16:48:37 +0800

    Dev: Updated the data loading script

--- preloadData.js
+++ preloadData.js
@@ -12,8 +12,18 @@ const db = mongoose.connection;
 db.on('open', async () => {
     console.log("Connection established.");
     const data = preloadData();
-    db.collection("paper").drop().catch(err => console.error("Failed to drop Paper"));
-    db.collection("scholar").drop().catch(err => console.error("Failed to drop Scholar"));
+    try {
+        await db.collection("paper").drop();
+        console.log("Dropped `paper` collection.");
+    } catch (err) {
+        console.error("Failed to drop Paper");
+    }
+    try {
+        await db.collection("scholar").drop();
+        console.log("Dropped `scholars` collection.")
+    } catch (err) {
+        console.error("Failed to drop Scholar");
+    }
     await ScholarSchema.insertMany(data[0]);
     console.log("All the scholars have been imported into the database.");
     await PaperSchema.insertMany(data[1]);
