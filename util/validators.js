8385fdec8034b8a075b0ef5e0dd8f29a2f3c55a6
mengtao97 <mtttt1997@outlook.com>
2019-12-11 17:23:55 +0800

    fix email regex

--- util/validators.js
+++ util/validators.js
@@ -5,18 +5,18 @@ module.exports.validateRegisterInput = (
 ) => {
     const errors = {};
     if (username.trim() === '') {
-        errors.username = 'Username must not be empty';
+        errors.username = '用户名不能为空';
     }
     if (email.trim() === '') {
-        errors.email = 'Email must not be empty';
+        errors.email = '邮箱不能为空';
     } else {
-        const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+        const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if (!email.match(regEx)) {
-            errors.email = 'Email must be a valid email address';
+            errors.email = '无效的邮箱地址';
         }
     }
     if (password === '') {
-        errors.password = 'Password must not empty';
+        errors.password = '密码不能为空';
     }
     return {
         errors,
@@ -27,15 +27,15 @@ module.exports.validateRegisterInput = (
 module.exports.validateLoginInput = (email, password) => {
     const errors = {};
     if (email.trim() === '') {
-        errors.email = 'Email must not be empty';
+        errors.email = '邮箱不能为空';
     } else {
         const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
         if (!email.match(regEx)) {
-            errors.email = 'Email must be a valid email address';
+            errors.email = '无效的邮箱地址';
         }
     }
     if (password.trim() === '') {
-        errors.password = 'Password must not be empty';
+        errors.password = '密码不能为空';
     }
 
     return {
