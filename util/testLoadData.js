d4f7693d739c763236f6f87a80d6c04e80ebbee9
chencongyong <ndsf@qq.com>
2019-12-17 23:14:09 +0800

    Update testLoadData.js

--- util/testLoadData.js
+++ util/testLoadData.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
 const content = fs.readFileSync("/home/ubuntu/data/coauthor_0.json", "utf8");
 const rawData = JSON.parse(content);
-console.log(rawData.slice(0,10));
+const keys = Object.keys(rawData);
+console.log(keys.slice(0,10));
