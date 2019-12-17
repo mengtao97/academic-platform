4be4e8a2fca33127aaeb3c6c035989d31cdf0318
mengtao97 <mtttt1997@outlook.com>
2019-12-18 00:42:35 +0800

    authentication

--- util/loadData.js
+++ util/loadData.js
@@ -21,43 +21,6 @@ module.exports = (file_path, mode) => {
             hIndex: author.h_index,
             avatar: "",
         }));
-<<<<<<< HEAD
-    const papersKeys = Object.keys(rawData.papers);
-    const papers = papersKeys.map(el => rawData.papers[el]);
-    const transformedPapers = papers.map(paper => {
-        const obj = {
-            authors: paper.authors,
-            _id: paper.id,
-            title: paper.title,
-        };
-        if (paper.issue)
-            obj['issue'] = paper.issue;
-        if (paper.lang)
-            obj['lang'] = paper.lang;
-        if (paper.year)
-            obj['year'] = paper.year;
-        if (paper.n_citation)
-            obj['nCitation'] = paper.n_citation;
-        if (paper.abstract)
-            obj['abstract'] = paper.abstract;
-        if (paper.keywords)
-            obj['keywords'] = paper.keywords;
-        if (paper.venue)
-            obj['venue'] = paper.venue.raw;
-        if (paper.page_start)
-            obj['pageEnde'] = paper.page_start;
-        if (paper.page_end)
-            obj['pageStart'] = paper.page_end;
-        if (paper.doi)
-            obj['doi'] = paper.doi;
-        if (paper.volume)
-            obj['volume'] = paper.volume;
-        if (paper.url)
-            obj['url'] = paper.url;
-        return obj;
-    });
-    return [transformedAuthors, transformedPapers];
-=======
         authors = null; // GC
         return transformedAuthors;
     } else {
@@ -108,5 +71,4 @@ module.exports = (file_path, mode) => {
         return transformedPapers;
     }
 
->>>>>>> 2c56047accb77549b523a1c225088ac2ea4873b2
 };
